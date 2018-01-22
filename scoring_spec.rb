aa8bf74bc52a088d987694517315e776746f171c
M. Kallstrom <28884778+m-kallstrom@users.noreply.github.com>
2018-01-21 18:40:58 -0600

    language name comes in as a variable with a symbol as its value

--- scoring_spec.rb
+++ scoring_spec.rb
@@ -65,58 +65,59 @@ RSpec.describe Scoring do
 
 
     context 'in Spanish' do
-      %w[A E I O U L N R S T].each do |letter|
+      let (:language) {:spanish}
+      %w[A E O S I U N L R T].each do |letter|
         it "scores a #{letter} as 1 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 1
         end
       end
 
-      %w[D G].each do |letter|
+      %w[C D G].each do |letter|
         it "scores a #{letter} as 2 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 2
         end
       end
 
-      %w[B C M P].each do |letter|
+      %w[B M P].each do |letter|
         it "scores a #{letter} as 3 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 3
         end
       end
 
-      %w[F H V W Y].each do |letter|
+      %w[F H V Y].each do |letter|
         it "scores a #{letter} as 4 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 4
         end
       end
 
-      %w[K].each do |letter|
+      %w[J].each do |letter|
         it "scores a #{letter} as 5 point" do
-          expect(subject('Spanish').score_letter(letter))
-            .to eq 5
+          expect(subject(language).score_letter(letter))
+            .to eq 6
         end
       end
 
-      %w[J X].each do |letter|
+      %w[K LL Ñ Q RR W X].each do |letter|
         it "scores a #{letter} as 8 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 8
         end
       end
 
-      %w[Q Z].each do |letter|
+      %w[Z].each do |letter|
         it "scores a #{letter} as 10 point" do
-          expect(subject('Spanish').score_letter(letter))
+          expect(subject(language).score_letter(letter))
             .to eq 10
         end
       end
 
       %w[1 -23 puppy false].each do |invalid_input|
         it "scores invalid input as 0 points" do 
-          expect(subject('Spanish').score_letter(invalid_input)).to eq 0
+          expect(subject(language).score_letter(invalid_input)).to eq 0
         end 
       end
     end 
