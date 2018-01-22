9a674a1e5096ed08187075d982a566a1e43c3d91
Dan Uber <duber@kennasecurity.com>
2018-01-22 11:27:31 -0600

    remove unneeded debugging statement

--- scoring_spec.rb
+++ scoring_spec.rb
@@ -10,9 +10,6 @@ RSpec.describe Scoring do
     context "in #{symbol.to_s.capitalize}" do
       subject { described_class.new symbol }
       language = Scoring::Languages.scoring_table_for(symbol)
-      if language.nil?
-        binding.pry
-      end
       language.keys.each do |key|
         score = language.fetch(key)
         key.each do |letter|
