2859e6772e32ca132aa5a020c9318e578e3a7cc5
M. Kallstrom <28884778+m-kallstrom@users.noreply.github.com>
2018-01-21 19:42:00 -0600

    edit Scoring class to take optional language parameter. Add initialize method and choose_cypher method to load the proper hash of letters/points. Refactor score_letter method to use letter-point cypher hash. All tests green.

--- scoring.rb
+++ scoring.rb
@@ -1,32 +1,95 @@
 # frozen_string_literal: true
 
 class Scoring
-  def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
+
+  def initialize(language=:english)
+    @language = language
+    self.choose_cypher
+  end 
+    
+  def choose_cypher
+
+    english_cypher = {
+      "A" => 1,
+      "B" => 3,
+      "C" => 3,
+      "D" => 2,
+      "E" => 1,
+      "F" => 4,
+      "G" => 2,
+      "H" => 4,
+      "I" => 1,
+      "J" => 8,
+      "K" => 5,
+      "L" => 1,
+      "M" => 3,
+      "N" => 1,
+      "O" => 1,
+      "P" => 3,
+      "Q" => 10,
+      "R" => 1,
+      "S" => 1,
+      "T" => 1,
+      "U" => 1,
+      "V" => 4,
+      "W" => 4,
+      "Y" => 4,
+      "X" => 8,
+      "Z" => 10,
+      }
+  
+    spanish_cypher = {
+      "A" =>  1,
+      "B" =>  3,
+      "C" =>  2,
+      "D" =>  2,
+      "E" =>  1,
+      "F" =>  4,
+      "G" =>  2,
+      "H" =>  4,
+      "I" =>  1,
+      "J" =>  6,
+      "K" =>  8,
+      "L" =>  1,
+      "LL" => 8,
+      "M" =>  3,
+      "N" =>  1,
+      "Ñ" =>  8,
+      "O" =>  1,
+      "P" =>  3,
+      "Q" =>  8,
+      "R" =>  1,
+      "RR" => 8,
+      "S" =>  1,
+      "T" =>  1,
+      "U" =>  1,
+      "V" =>  4,
+      "W" =>  8,
+      "X" =>  8,
+      "Y" =>  4,
+      "Z" =>  10,
+  
+      }
+
+    if @language == :spanish 
+      @scoring_cypher = spanish_cypher
     else
-      0
-    end
+      @scoring_cypher = english_cypher 
+    end  
+  end 
+
+
+  def score_letter(letter)
+    @scoring_cypher[letter] ? @scoring_cypher[letter] : 0   
   end
+
 end
 
 =begin 
 
 Business Logic:
   * New language feature needs to work without changing the way the method is called!
-  - INPUT: Letter, optionally a language name
+  - INPUT: Letter as a string, optionally a language name as a symbol
   - OUTPUT: Integer for the score of that letter, 0 if invalid
   - add initialize method with an instance variable for what language to be used 
   - default value should be current language (English) with option for other languages 
@@ -50,9 +113,11 @@ Refactoring:
   - cypher hash for each language, set on initialization
   - why do some lines have 'return' and others don't?
   - separation of concerns:  should another method first check if the input is in the current alphabet and upcase it?
+  - more separation of concerns: does language justify its own Class? 
 
 Performance:
-  - checking arrays for inclusion vs key/value lookup. I suspect the latter is faster (especially for letters like Q and Z) but can do tests to confirm 
+  - checking arrays for inclusion vs key/value lookup. I suspect the latter is faster (especially for letters like Q and Z) but can do tests to confirm
+  - case statements vs if / else. Which has the advantage as you add lanuages? We gotta globalize Scrobly before those Scrabble posers can corner the market!
 
 
 =end 
