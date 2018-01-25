e90fd2682d932cf47b5e63861dd800a4de524bc4
Joshua Lugo <atwfaj@gmail.com>
2018-01-24 21:25:31 -0600

    pull out language specific letter-scores and combine like scores into one list

--- scoring.rb
+++ scoring.rb
@@ -9,24 +9,26 @@ class Scoring
   end
 
   def score_letter(letter)
+    #common letter-scores extracted
+    return 1 if %w[A E I O U L N R S T].include?(letter)
+    return 2 if %w[D G].include?(letter)
+    return 3 if %w[B M P].include?(letter)
+    return 4 if %w[F H V Y].include?(letter)
+    return 8 if %w[X].include?(letter)
+    return 10 if %w[Z].include?(letter)
+
+    #language specific letter-scores
     if @language == :english
-      return 1 if %w[A E I O U L N R S T].include?(letter)
-      return 2 if %w[D G].include?(letter)
-      return 3 if %w[B C M P].include?(letter)
-      return 4 if %w[F H V W Y].include?(letter)
+      return 3 if %w[C].include?(letter)
+      return 4 if %w[W].include?(letter)
       return 5 if %w[K].include?(letter)
-      return 8 if %w[J X].include?(letter)
-      return 10 if %w[Q Z].include?(letter)
-      return 0
+      return 8 if %w[J].include?(letter)
+      return 10 if %w[Q].include?(letter)
     elsif @language == :spanish
-      return 1 if %w[A E O S I U N L R T].include?(letter)
-      return 2 if %w[C D G].include?(letter)
-      return 3 if %w[B M P].include?(letter)
-      return 4 if %w[F H V Y].include?(letter)
+      return 2 if %w[C].include?(letter)
       return 6 if %w[J].include?(letter)
-      return 8 if %w[K LL Ñ Q RR W X].include?(letter)
-      return 10 if %w[Z].include?(letter)
-      return 0
+      return 8 if %w[K LL Ñ Q RR W].include?(letter)
     end
+    return 0
   end
 end
