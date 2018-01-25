712105e0558cfcb5bbe0e97d07941c302ba37bdd
Joshua Lugo <atwfaj@gmail.com>
2018-01-24 20:12:16 -0600

    make statements one line and return explicitly

--- scoring.rb
+++ scoring.rb
@@ -2,22 +2,14 @@
 
 class Scoring
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
+    return 1 if %w[A E I O U L N R S T].include?(letter)
+    return 2 if %w[D G].include?(letter)
+    return 3 if %w[B C M P].include?(letter)
+    return 4 if %w[F H V W Y].include?(letter)
+    return 5 if %w[K].include?(letter)
+    return 8 if %w[J X].include?(letter)
+    return 10 if %w[Q Z].include?(letter)
+    return 0 else
     end
   end
 end
