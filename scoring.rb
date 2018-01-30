da5cedb2e6962db9fd670f2e6a81290d05fc2f2c
graham <gdh56@cornell.edu>
2018-01-29 19:35:58 -0600

    add loading spec and add mexico and test cases

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
+require_relative "json_score_loading"
+
+# So I kind of went a little overboard here. I basically thought of this as a mapping of single letters to a score that
+# was defined by what country you are in. I didn't really like implementing it as hard coded if statements because if
+# any changes needed to be made then you'd have to make code changes. That sounds annoying to me. Though the tests are
+# still hard coded.
+# Basically I stuck a loading file in here so there would be a decoupled loader that was separated from the scoring
+# numbers themselves
+# I am not sure this is the most idiomatic way of doing the project but this is my first experience with ruby
+
 class Scoring
+
+  def initialize(language)
+    score_loader = JSONScoreLoading.new
+    # Ideally this could be subbed out for any other score loader
+    @scoring_map = score_loader.get_scoring_map_by_language(language)
+  end
+
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
-    end
+    @scoring_map[letter.upcase]
   end
 end
--- scoring_spec.rb
+++ scoring_spec.rb
@@ -3,10 +3,14 @@
 require_relative 'scoring'
 
 RSpec.describe Scoring do
-  subject { described_class.new }
-
+  # I think the tests (And the scoring) should be done by country and not necessarily just by language due to dialect.
+  # This is more relevant for Spanish but different letters differ in frequency by dialect.
+  # I am not sure if there is a better way to refactor this and I was thinking of having this load countries based
+  # on the loading type and not hard coding the letter scores but that is probably ok for this case.
   describe '#score_letter' do
-    context 'in English' do
+    context 'in Canada' do
+      subject { described_class.new("CANADA") }
+
       %w[A E I O U L N R S T].each do |letter|
         it "scores a #{letter} as 1 point" do
           expect(subject.score_letter(letter))
@@ -55,10 +59,88 @@ RSpec.describe Scoring do
             .to eq 10
         end
       end
+    # I thought I should have some sort of test around the letter not being scoreable.
+    # I think it should be an exception but this is what the spec had
+      %w[Ó %].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(subject.score_letter(letter))
+              . to eq 0
+        end
+      end
+
+    # In case of lowercase input, I think the scoring should be the same
+    # one letter from each category
+      %w[a d b f j k z].each do |letter|
+        it "scores lowercase #{letter} the same as uppercase" do
+          expect(subject.score_letter(letter))
+              . to eq subject.score_letter(letter.upcase)
+        end
+      end
     end
 
-    context 'in Spanish' do
-      pending 'deliverable'
+    context 'in Mexico' do
+      subject { described_class.new("MEXICO") }
+
+      %w[A E O S I U N L R T].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(subject.score_letter(letter))
+              .to eq 1
+        end
+      end
+
+      %w[D G].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(subject.score_letter(letter))
+              .to eq 2
+        end
+      end
+
+      %w[B M P].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(subject.score_letter(letter))
+              .to eq 3
+        end
+      end
+
+      %w[F H V Y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(subject.score_letter(letter))
+              .to eq 4
+        end
+      end
+
+      %w[J].each do |letter|
+        it "scores a #{letter} as 5 point" do
+          expect(subject.score_letter(letter))
+              .to eq 5
+        end
+      end
+
+      %w[K LL Ñ Q RR W X].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(subject.score_letter(letter))
+              .to eq 8
+        end
+      end
+
+      %w[Z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(subject.score_letter(letter))
+              .to eq 10
+        end
+      end
+        %w[Ó %].each do |letter|
+          it "scores a #{letter} as 0 point" do
+            expect(subject.score_letter(letter))
+             . to eq 0
+          end
+        end
+        %w[a d b f j k z].each do |letter|
+          it "scores lowercase #{letter} the same as uppercase" do
+            expect(subject.score_letter(letter))
+            . to eq subject.score_letter(letter.upcase)
+          end
+        end
+      end
     end
   end
-end
