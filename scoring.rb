91287224d6e5b75fd5108b1c12f71513fb87dece
EC2 Default User <ec2-user@ip-172-31-46-245.ec2.internal>
2018-04-17 14:23:16 +0000

    Take Home Challenge Complete

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,58 @@
 # frozen_string_literal: true
+class Scoring 
 
-class Scoring
-  def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
-    end
+  def initialize(language)
+    @language = language.downcase #case insensitive
   end
-end
+  
+  def score_letter(letter)
+    letter = letter.upcase #case insensitive 
+    
+    # English Metric Chosen  
+    if @language.eql?(:english)
+      if %w[A E I O U L N R S T].include?(letter)
+        return 1
+      elsif %w[D G].include?(letter)
+        2
+      elsif %w[B C M P].include?(letter)
+        3
+      elsif %w[F H V W Y].include?(letter)
+        4
+      elsif %w[K].include?(letter)
+        5
+      elsif %w[J X].include?(letter)
+        8
+      elsif %w[Q Z].include?(letter)
+        10
+      else
+        0
+      end # endif for english condition
+      
+    # Spanish Metric Chosen  
+    elsif @language.eql?(:spanish)
+      if %w[A E O S I U N L R T].include?(letter)
+        1
+      elsif %w[C D G].include?(letter)
+        2
+      elsif %w[B M P].include?(letter)
+        3
+      elsif %w[F H V Y].include?(letter)
+        4
+      elsif %w[J].include?(letter)
+        6
+      elsif %w[K LL Ñ Q RR W X].include?(letter)
+        8
+      elsif %w[Z].include?(letter)
+        10
+      else
+        0
+      end # endif for spanish condition
+      
+    end  # endif for language logic/toggler
+  end   # end score_letter method
+end    # end Scoring class
+
+# small test to ensure correctness before moving onto RSpec Testing
+# language = :SPANISH
+# scorer = Scoring.new(language).score_letter('R')
+# puts scorer
--- scoring_spec.rb
+++ scoring_spec.rb
@@ -6,59 +6,214 @@ RSpec.describe Scoring do
   subject { described_class.new }
 
   describe '#score_letter' do
+    # Test English
     context 'in English' do
+      # Test in English for Uppercase
       %w[A E I O U L N R S T].each do |letter|
         it "scores a #{letter} as 1 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 1
         end
       end
 
       %w[D G].each do |letter|
         it "scores a #{letter} as 2 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 2
         end
       end
 
       %w[B C M P].each do |letter|
         it "scores a #{letter} as 3 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 3
         end
       end
 
       %w[F H V W Y].each do |letter|
         it "scores a #{letter} as 4 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 4
         end
       end
 
       %w[K].each do |letter|
         it "scores a #{letter} as 5 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 5
         end
       end
 
       %w[J X].each do |letter|
         it "scores a #{letter} as 8 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 8
         end
       end
 
       %w[Q Z].each do |letter|
         it "scores a #{letter} as 10 point" do
-          expect(subject.score_letter(letter))
+          expect(described_class.new(:english).score_letter(letter))
             .to eq 10
         end
       end
-    end
+      # End Test in English for Uppercase
+      
+      # Test in English for Lowecase
+      %w[a e i o u l n r s t].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 1
+        end
+      end
 
+      %w[d g].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 2
+        end
+      end
+
+      %w[b c m p].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 3
+        end
+      end
+
+      %w[f h v w y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 4
+        end
+      end
+
+      %w[k].each do |letter|
+        it "scores a #{letter} as 5 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 5
+        end
+      end
+
+      %w[j x].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 8
+        end
+      end
+
+      %w[q z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(described_class.new(:english).score_letter(letter))
+            .to eq 10
+        end
+      end
+      # End Test in English for Lowercase
+    end # End Test in English 
+
+    # Test Spanish
+    # Test Spanish in Uppercase
     context 'in Spanish' do
-      pending 'deliverable'
-    end
-  end
+      %w[A E O S I U N L R T].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 1
+        end
+      end
+      
+      %w[C D G].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 2
+        end
+      end
+      
+      %w[B M P].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 3
+        end
+      end
+
+      %w[F H V Y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 4
+        end
+      end
+      
+      %w[J].each do |letter|
+        it "scores a #{letter} as 6 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 6
+        end
+      end
+      
+      %w[K LL Ñ Q RR W X].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 8
+        end
+      end
+      
+      %w[Z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 10
+        end
+      end
+      # End Test in Spanish for Uppercase
+      
+      # Start Test for Spanish in Lowercase
+      %w[a e o s i u n l r t].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 1
+        end
+      end
+      
+      %w[c d g].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 2
+        end
+      end
+      
+      %w[b m p].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 3
+        end
+      end
+
+      %w[f h v y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 4
+        end
+      end
+      
+      %w[j].each do |letter|
+        it "scores a #{letter} as 6 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 6
+        end
+      end
+      
+      %w[k ll ñ q rr w x].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 8
+        end
+      end
+      
+      %w[z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(described_class.new(:Spanish).score_letter(letter))
+            .to eq 10
+        end
+      end
+      # End Test for Spanish in Lowercase
+    end # End Test in Spanish
+  end 
 end
