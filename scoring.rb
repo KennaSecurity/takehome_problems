067284666d4507a05498f329049cc9b5436f6952
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2018-04-25 19:32:51 -0500

    move logic into private methods separately for english scoring and spanish scoring

--- scoring.rb
+++ scoring.rb
@@ -9,25 +9,35 @@ class Scoring
 
   def score_letter(letter)
     if language.downcase == :spanish
-      return 1 if %w[A E O S I U N L R T].include?(letter)
-      return 2 if %w[C D G].include?(letter)
-      return 3 if %w[B M P].include?(letter)
-      return 4 if %w[F H V Y].include?(letter)
-      return 6 if %w[J].include?(letter)
-      return 8 if %w[K LL Ñ Q RR W X].include?(letter)
-      return 10 if %w[Z].include?(letter)
-      0
+      score_spanish_letter(letter)
     elsif language.downcase == :english
-      return 1 if %w[A E I O U L N R S T].include?(letter)
-      return 2 if %w[D G].include?(letter)
-      return 3 if %w[B C M P].include?(letter)
-      return 4 if %w[F H V W Y].include?(letter)
-      return 5 if %w[K].include?(letter)
-      return 8 if %w[J X].include?(letter)
-      return 10 if %w[Q Z].include?(letter)
-      0
+      score_english_letter(letter)
     else
       puts 'Please enter a valid language'
     end
+  end
+
+  private
+
+  def score_spanish_letter(letter)
+    return 1 if %w[A E O S I U N L R T].include?(letter)
+    return 2 if %w[C D G].include?(letter)
+    return 3 if %w[B M P].include?(letter)
+    return 4 if %w[F H V Y].include?(letter)
+    return 6 if %w[J].include?(letter)
+    return 8 if %w[K LL Ñ Q RR W X].include?(letter)
+    return 10 if %w[Z].include?(letter)
+    0
+  end
+
+  def score_english_letter(letter)
+    return 1 if %w[A E I O U L N R S T].include?(letter)
+    return 2 if %w[D G].include?(letter)
+    return 3 if %w[B C M P].include?(letter)
+    return 4 if %w[F H V W Y].include?(letter)
+    return 5 if %w[K].include?(letter)
+    return 8 if %w[J X].include?(letter)
+    return 10 if %w[Q Z].include?(letter)
+    0
   end
 end
