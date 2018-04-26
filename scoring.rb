ae169ba43a308f2d65735e2815089462509a07e2
Anca Ciascaiu <anca_ciascaiu@yahoo.com>
2018-04-25 19:06:31 -0500

    reduce if-elsif-else statements to one liners for readability

--- scoring.rb
+++ scoring.rb
@@ -11,23 +11,14 @@ class Scoring
     if language.downcase == :spanish
 
     elsif language.downcase == ''
-      if %w[A E I O U L N R S T].include?(letter)
-        return 1
-      elsif %w[D G].include?(letter)
-        2
-      elsif %w[B C M P].include?(letter)
-        3
-      elsif %w[F H V W Y].include?(letter)
-        return 4
-      elsif %w[K].include?(letter)
-        5
-      elsif %w[J X].include?(letter)
-        8
-      elsif %w[Q Z].include?(letter)
-        10
-      else
-        0
-      end
+      return 1 if %w[A E I O U L N R S T].include?(letter)
+      return 2 if %w[D G].include?(letter)
+      return 3 if %w[B C M P].include?(letter)
+      return 4 if %w[F H V W Y].include?(letter)
+      return 5 if %w[K].include?(letter)
+      return 8 if %w[J X].include?(letter)
+      return 10 if %w[Q Z].include?(letter)
+      0
     else
       puts 'Please enter a valid language'
     end
