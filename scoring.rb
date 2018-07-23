0390c2820916d3f72d65a97c3504c29733d23ed7
Clint Thacker <couscous@gmail.com>
2018-07-23 09:46:11 -0500

    Refactor English scoring so that scores/letter are stored in a hash for faster lookup and can be initialized with a language other than English

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,34 @@
 # frozen_string_literal: true
 
+
 class Scoring
-  def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
-    end
+  attr_reader :language
+
+# benchmarking tests indicate that peformance is faster with the scores as keys
+# also this is more legible
+  ENGLISH_TILE_SET = {
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => %w[K],
+    8 => %w[J X],
+    10 => %w[Q Z]
+  }
+
+# defaults to english if no language parameter is provided upon initialization
+  def initialize(args={})
+    @language = args[:language] || :english
   end
+
+# considered returning an ArgumentError here instead of 0 but decided against it since the 0 provides greater flexibility as features are built out
+  def score_letter(letter)
+    ENGLISH_TILE_SET.each do |score, tiles|
+      if tiles.include?(letter)
+        return score
+      end
+    end
+    return 0
+  end
+
 end
