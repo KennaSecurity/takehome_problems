f03f8a3f78267b4993d0a7851013f57d666fcd62
Zack Frank <frank.zack@gmail.com>
2018-11-13 15:57:03 -0600

    Add tests for Spanish point values

--- scoring_spec.rb
+++ scoring_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require_relative 'scoring'
 
 RSpec.describe Scoring do
@@ -58,7 +57,62 @@ RSpec.describe Scoring do
     end
 
     context 'in Spanish' do
-      pending 'deliverable'
+      
+      %w[A E O S I U N L R T].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(subject.score_letter(letter))
+            .to eq 1
+        end
+      end
+
+      %w[C D G].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 2
+        end
+      end
+
+      %w[B M P].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 3
+        end
+      end
+
+      %w[F H V Y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 4
+        end
+      end
+
+      %w[J].each do |letter|
+        it "scores a #{letter} as 5 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 6
+        end
+      end
+      
+      %w[K LL Ñ Q RR W X].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 8
+        end
+      end
+
+      %w[Q Z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          skip
+          expect(subject.score_letter(letter))
+            .to eq 10
+        end
+      end
+
     end
   end
 end
