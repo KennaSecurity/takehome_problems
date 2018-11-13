d8e23f69af93ba6681574b89c6ff003892e6975d
Zack Frank <frank.zack@gmail.com>
2018-11-13 17:37:53 -0600

    Add language case statement to Scoring#score_letter for :english and :spanish -- :spanish case still empty -- existing logic inside of :english case still passing tests

--- scoring.rb
+++ scoring.rb
@@ -8,22 +8,26 @@ class Scoring
   end
 
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
+    case language
+    when :english
+      if %w[A E I O U L N R S T].include?(letter)
+        return 1
+      elsif %w[D G].include?(letter)
+        2
+      elsif %w[B C M P].include?(letter)
+        3
+      elsif %w[F H V W Y].include?(letter)
+        return 4
+      elsif %w[K].include?(letter)
+        5
+      elsif %w[J X].include?(letter)
+        8
+      elsif %w[Q Z].include?(letter)
+        10
+      else
+        0
+      end
+    when :spanish
     end
   end
 end
