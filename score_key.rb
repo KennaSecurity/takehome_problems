d51d15df414e216d6f405cba07ebe2cae2173780
Luis  Senior <heybluguy@yahoo.com>
2018-11-18 17:44:57 -0600

    Fixed indentation.

--- score_key.rb
+++ score_key.rb
@@ -1,19 +1,19 @@
 ENGLISH = {
-    1 => %w[A E I O U L N R S T],
-    2 => %w[D G],
-    3 => %w[B C M P],
-    4 => %w[F H V W Y],
-    5 => %w[K],
-    8 => %w[J X],
-    10 => %w[Q Z]
-  }
+  1 => %w[A E I O U L N R S T],
+  2 => %w[D G],
+  3 => %w[B C M P],
+  4 => %w[F H V W Y],
+  5 => %w[K],
+  8 => %w[J X],
+  10 => %w[Q Z]
+}
 
-  SPANISH = {
-    1 => %w[A E O S I U N L R T],
-    2 => %w[C D G],
-    3 => %w[B M P],
-    4 => %w[F H V Y],
-    6 => %w[J],
-    8 => %w[K LL Ñ Q RR W X],
-    10 => %w[Z]
-  }
+SPANISH = {
+  1 => %w[A E O S I U N L R T],
+  2 => %w[C D G],
+  3 => %w[B M P],
+  4 => %w[F H V Y],
+  6 => %w[J],
+  8 => %w[K LL Ñ Q RR W X],
+  10 => %w[Z]
+}
