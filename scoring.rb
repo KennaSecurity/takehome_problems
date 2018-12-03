c8497d0fe58595292b57c7a8d5b8e68790ef93dc
Luis  Senior <heybluguy@yahoo.com>
2018-12-03 15:57:05 -0600

    optimized performance

--- scoring.rb
+++ scoring.rb
@@ -6,12 +6,23 @@ class Scoring
     @score_key = handle_language(language)
   end
 
-  def score_letter(letter)
-    @score_key.each {|key, value| return key if value.include?(letter.upcase)}
-  end
-
   def handle_language(language)
     return SPANISH if language == :spanish
     language
+  end
+
+  def score_letter(letter)
+    fixed_score[letter.upcase]
+  end
+
+  def fixed_score
+    return @fixed_score if @fixed_score
+    @fixed_score = {}
+    @score_key.each do |score , letters|
+      letters.each do |letter|
+        @fixed_score[letter]= score
+      end
+    end
+    @fixed_score
   end
 end
