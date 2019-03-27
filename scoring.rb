db5acd02146d361ea53a855b2476a3e2a99bbbc2
lafesh <ginafedder@yahoo.com>
2019-03-27 16:34:19 -0600

    refactored score_letter by using constants for languages, created initialize method for languges

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,56 @@
 # frozen_string_literal: true
 
 class Scoring
+  attr_accessor :language
+
+  #created constant hashes for each language option
+  ENGLISH = {
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => %w[K],
+    8 => %w[J X],
+    10 => %w[Q Z]
+  }
+
+  SPANISH = {
+    1 => %w[A E O S I U N L R T],
+    2 => %w[C D G],
+    3 => %w[B M P],
+    4 => %w[F H V Y],
+    6 => %w[J],
+    8 => %w[K LL Ñ Q RR W X],
+    10 => %w[Z]
+  }
+
+  #initialized language with a default value of :english
+  def initialize(language == :english)
+    @language = language
+  end
+
+  #simplified method by using constants, added an error message for wrong input
+  #too repetitive -> find the language input in our constant set??
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
+    if @language == :english 
+      ENGLISH.map do |point, l| 
+        if l.include?(letter)
+          return point
+        else
+          0
+        end
+      end
+
+    elsif @language == :spanish
+      SPANISH.map do |point, l| 
+        if l.include?(letter)
+          return point
+        else
+          0
+        end
+
     else
-      0
+      return "We do not have this language available yet"  
     end
   end
 end
