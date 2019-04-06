3e8b7685e5c006f1f7dd270c44c9be819ee4afa4
lafesh <ginafedder@yahoo.com>
2019-04-06 11:47:29 -0600

    adjusted tests to new argument in score_letter

--- scoring_spec.rb
+++ scoring_spec.rb
@@ -21,49 +21,49 @@ RSpec.describe Scoring do
     context 'in English' do
       %w[A E I O U L N R S T].each do |letter|
         it "scores a #{letter} as 1 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 1
         end
       end
 
       %w[D G].each do |letter|
         it "scores a #{letter} as 2 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 2
         end
       end
 
       %w[B C M P].each do |letter|
         it "scores a #{letter} as 3 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 3
         end
       end
 
       %w[F H V W Y].each do |letter|
         it "scores a #{letter} as 4 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 4
         end
       end
 
       %w[K].each do |letter|
         it "scores a #{letter} as 5 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 5
         end
       end
 
       %w[J X].each do |letter|
         it "scores a #{letter} as 8 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 8
         end
       end
 
       %w[Q Z].each do |letter|
         it "scores a #{letter} as 10 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 10
         end
       end
@@ -72,52 +72,52 @@ RSpec.describe Scoring do
     #I am not familiar with testing at all so I just added the Spanish version for now
     context 'in Spanish' do
       subject {described_class.new(:spanish)}
-      
+         
       %w[A E O S I U N L R T].each do |letter|
         it "scores a #{letter} as 1 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 1
         end
       end
 
       %w[C D G].each do |letter|
         it "scores a #{letter} as 2 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 2
         end
       end
 
       %w[B M P].each do |letter|
         it "scores a #{letter} as 3 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 3
         end
       end
 
       %w[F H V Y].each do |letter|
         it "scores a #{letter} as 4 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 4
         end
       end
 
       %w[J].each do |letter|
         it "scores a #{letter} as 5 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 6
         end
       end
 
       %w[K LL Ñ Q RR W X].each do |letter|
         it "scores a #{letter} as 8 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 8
         end
       end
 
       %w[Z].each do |letter|
         it "scores a #{letter} as 10 point" do
-          expect(subject.score_letter(subject, letter))
+          expect(subject.score_letter(subject.find_version, letter))
             .to eq 10
         end
       end
