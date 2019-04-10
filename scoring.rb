98909046b5a8aa45b80999da17e56c42a9fc7e51
swgoodman <spencerwgoodman@icloud.com>
2019-04-09 20:28:36 -0500

    Adjust Languages hash object spacing

--- scoring.rb
+++ scoring.rb
@@ -2,27 +2,27 @@
 
 class Scoring
 
-      # Hash object containing language letter-value pairs
-      Languages = {
-        :spanish => {
-          %w[A E O S I U N L R T] => 1,
-          %w[C D G] => 2,
-          %w[B M P] => 3,
-          %w[F H V Y] => 4,
-          %w[J] => 6,
-          %w[K LL Ñ Q RR W X] => 8,
-          %w[Z] => 10
-        },
-        :english  => {
-          %w[E A I O N R T L S U] => 1,
-          %w[D G] => 2,
-          %w[B C M P] => 3,
-          %w[F H V W Y] => 4,
-          %w[K] => 5,
-          %w[J X] => 8,
-          %w[Q Z] => 10
-        }
-      }
+  # Hash object containing language letter-value pairs
+  Languages = {
+    :spanish => {
+      %w[A E O S I U N L R T] => 1,
+      %w[C D G] => 2,
+      %w[B M P] => 3,
+      %w[F H V Y] => 4,
+      %w[J] => 6,
+      %w[K LL Ñ Q RR W X] => 8,
+      %w[Z] => 10
+    },
+    :english  => {
+      %w[E A I O N R T L S U] => 1,
+      %w[D G] => 2,
+      %w[B C M P] => 3,
+      %w[F H V W Y] => 4,
+      %w[K] => 5,
+      %w[J X] => 8,
+      %w[Q Z] => 10
+    }
+  }
 
   def score_letter(letter)
     if %w[A E I O U L N R S T].include?(letter)
