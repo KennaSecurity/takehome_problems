383d6540418679549096c7b8d6a6f9c19614a44a
swgoodman <spencerwgoodman@icloud.com>
2019-04-09 21:53:25 -0500

    Refactored score letter class method
    Updated method to work with new instance variables, search the provided hash, and return the value contained in the returned array, or ‘0’ if no match is found.

--- scoring.rb
+++ scoring.rb
@@ -9,23 +9,14 @@ class Scoring
     @point_table = Language.new(language).get
   end
 
+  # Score Letter, searches provided hash and returns found relevant value or 0 
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
+    table_entry = self.point_table.find {|k,v| k.include?(letter.upcase)}
+    
+    if table_entry != nil
+      return table_entry[1]
     else
-      0
+      return 0
     end
   end
 
