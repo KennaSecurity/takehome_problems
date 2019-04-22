57fcbd2d7ba918befb742875ba6a4b802211e4fb
BTWitsK <BTWitsK@Gmail.com>
2019-04-22 17:43:46 -0500

    add spanish scoring and tests

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,51 @@
 # frozen_string_literal: true
 
 class Scoring
+  attr_accessor :language
+  def initialize(language = 'english')
+    @language = language
+  end
+
   def score_letter(letter)
-    if %w[A E I O U L N R S T].include?(letter)
-      return 1
-    elsif %w[D G].include?(letter)
-      2
-    elsif %w[B C M P].include?(letter)
-      3
-    elsif %w[F H V W Y].include?(letter)
-      return 4
-    elsif %w[K].include?(letter)
-      5
-    elsif %w[J X].include?(letter)
-      8
-    elsif %w[Q Z].include?(letter)
-      10
-    else
-      0
+    if language == 'english'
+
+      if %w[A E I O U L N R S T].include?(letter)
+        return 1
+      elsif %w[D G].include?(letter)
+        return 2
+      elsif %w[B C M P].include?(letter)
+        return 3
+      elsif %w[F H V W Y].include?(letter)
+        return 4
+      elsif %w[K].include?(letter)
+        return 5
+      elsif %w[J X].include?(letter)
+        return 8
+      elsif %w[Q Z].include?(letter)
+        return 10
+      else
+        return 0
+      end
+
+    elsif language == 'spanish'
+
+      if %w[A E I O U L N R S T].include?(letter)
+        return 1
+      elsif %w[C D G].include?(letter)
+        return 2
+      elsif %w[B M P].include?(letter)
+        return 3
+      elsif %w[F H V Y].include?(letter)
+        return 4
+      elsif %w[J].include?(letter)
+        return 6
+      elsif %w[K LL Ñ Q RR W X].include?(letter)
+        return 8
+      elsif %w[Z].include?(letter)
+        10
+      else
+        return 0
+      end
     end
   end
 end
--- scoring_spec.rb
+++ scoring_spec.rb
@@ -2,6 +2,7 @@
 
 require_relative 'scoring'
 
+
 RSpec.describe Scoring do
   subject { described_class.new }
 
@@ -58,7 +59,54 @@ RSpec.describe Scoring do
     end
 
     context 'in Spanish' do
-      pending 'deliverable'
+      %w[A E I O U L N R S T].each do |letter|
+        it "scores a #{letter} as 1 point" do
+          expect(subject.score_letter(letter))
+              .to eq 1
+        end
+      end
+
+      %w[C D G].each do |letter|
+        it "scores a #{letter} as 2 point" do
+          expect(subject.score_letter(letter))
+              .to eq 2
+        end
+      end
+
+      %w[B M P].each do |letter|
+        it "scores a #{letter} as 3 point" do
+          expect(subject.score_letter(letter))
+              .to eq 3
+        end
+      end
+
+      %w[F H V Y].each do |letter|
+        it "scores a #{letter} as 4 point" do
+          expect(subject.score_letter(letter))
+              .to eq 4
+        end
+      end
+
+      %w[J].each do |letter|
+        it "scores a #{letter} as 5 point" do
+          expect(subject.score_letter(letter))
+              .to eq 6
+        end
+      end
+
+      %w[K LL Ñ Q RR W X].each do |letter|
+        it "scores a #{letter} as 8 point" do
+          expect(subject.score_letter(letter))
+              .to eq 8
+        end
+      end
+
+      %w[Z].each do |letter|
+        it "scores a #{letter} as 10 point" do
+          expect(subject.score_letter(letter))
+              .to eq 10
+        end
+      end
     end
   end
 end
