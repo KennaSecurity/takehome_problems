e068ddb40c640cb1529b60ebe6b557cff1ea1c85
derek2041 <derek2041@gmail.com>
2019-04-23 18:23:45 -0400

    now supports spanish letters
    added support for spanish letters as well as tests for spanish letters

--- scoring.rb
+++ scoring.rb
@@ -1,23 +1,60 @@
 # frozen_string_literal: true
 
 class Scoring
+  def initialize(*args)
+    @language = :english # language is english by default
+    if args.length == 1 # if user specifies a language
+      @language = args[0] # set language
+    end
+  end
+
   def score_letter(letter)
+    if @language == :english
+      score_english(letter)
+    elsif @language == :spanish
+      score_spanish(letter)
+    else
+      puts "\e[31mcurrently does not support the #{@language} language\e[0m"
+    end
+  end
+
+  def score_english(letter)
     if %w[A E I O U L N R S T].include?(letter)
       return 1
     elsif %w[D G].include?(letter)
-      2
+      return 2
     elsif %w[B C M P].include?(letter)
-      3
+      return 3
     elsif %w[F H V W Y].include?(letter)
       return 4
     elsif %w[K].include?(letter)
-      5
+      return 5
     elsif %w[J X].include?(letter)
-      8
+      return 8
     elsif %w[Q Z].include?(letter)
-      10
+      return 10
     else
-      0
+      return 0
+    end
+  end
+
+  def score_spanish(letter)
+    if %w[A E O S I U N L R T].include?(letter)
+      return 1
+    elsif %w[C D G].include?(letter)
+      return 2
+    elsif %w[B M P].include?(letter)
+      return 3
+    elsif %w[F H V Y].include?(letter)
+      return 4
+    elsif %w[J].include?(letter)
+      return 6
+    elsif %w[K LL Ñ Q RR W X].include?(letter)
+      return 8
+    elsif %w[Z].include?(letter)
+      return 10
+    else
+      return 0
     end
   end
 end
