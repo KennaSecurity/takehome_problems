2ba4f4fe7ccc8ffe5f7bd04dad56aefd90f40c71
Sue Jeong Hur <shur@Sues-MacBook-Pro.local>
2019-06-14 17:03:07 -0700

    faster with tests

--- python/scoring.py
+++ python/scoring.py
@@ -1,39 +1,23 @@
 class Scoring:
-    def __init__(self, language=''):
+    def __init__(self, language='english'):
         self.language = language
+        self.englookup = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1,
+        'T': 1, 'D': 2, 'G': 2, 'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5, 'J': 8, 'X': 8, 'Q': 10, 'Z': 10}
+        self.splookup = {'A': 1, 'E': 1, 'O': 1, 'S': 1, 'I': 1, 'U': 1, 'N': 1, 'L': 1, 'R': 1, 'T': 1,
+        'C': 2, 'D': 2, 'G': 2, 'B': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4, 'V': 4, 'Y': 4, 'J': 6, 'K': 8,
+        'LL': 8, 'Ñ': 8, 'Q': 8, 'RR': 8, 'W': 8, 'X': 8, 'Z': 10}
 
     def score_letter(self, letter):
-        if self.language == 'spanish':
-            if letter in ['A', 'E', 'O', 'S', 'I', 'U', 'N', 'L', 'R', 'T']:
-                return 1
-            elif letter in ['C', 'D', 'G']:
-                return 2
-            elif letter in ['B', 'M', 'P']:
-                return 3
-            elif letter in ['F', 'H', 'V', 'Y']:
-                return 4
-            elif letter in ['J']:
-                return 5
-            elif letter in ['K', 'LL', 'Ñ', 'Q' 'RR', 'W', 'X']:
-                return 8
-            elif letter in ['Z']:
-                return 10
+        if self.language == 'english':
+            if letter in self.englookup:
+                return self.englookup[letter]
+            else:
+                return 0
+        elif self.language == 'spanish':
+            if letter in self.splookup:
+                return self.splookup[letter]
             else:
                 return 0
         else:
-            if letter in ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']:
-                return 1
-            elif letter in ['D', 'G']:
-                return 2
-            elif letter in ['B', 'C', 'M', 'P']:
-                return 3
-            elif letter in ['F', 'H', 'V', 'W', 'Y']:
-                return 4
-            elif letter in ['K']:
-                return 5
-            elif letter in ['J', 'X']:
-                return 8
-            elif letter in ['Q', 'Z']:
-                return 10
-            else:
-                return 0
+            return "Unsupported Language"
--- python/scoring_test.py
+++ python/scoring_test.py
@@ -42,10 +42,45 @@ class TestEnglishScoring(unittest.TestCase):
             with self.subTest(letter=letter):
                 self.assertEqual(self.subject.score_letter(letter), 10)
 
-
 class TestSpanishScoring(unittest.TestCase):
     def setUp(self):
-        self.subject = Scoring()
+        self.subject = Scoring('spanish')
+
+    def test_one_point(self):
+        for letter in ['A', 'E', 'O', 'S', 'I', 'U', 'N', 'L', 'R', 'T']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 1)
+
+    def test_two_point(self):
+        for letter in ['C', 'D', 'G']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 2)
+
+    def test_three_point(self):
+        for letter in ['B', 'M', 'P']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 3)
+
+    def test_four_point(self):
+        for letter in ['F', 'H', 'V', 'Y']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 4)
+
+    def test_six_point(self):
+        for letter in ['J']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 6)
+
+    def test_eight_point(self):
+        for letter in ['K', 'LL', 'Ñ', 'Q', 'RR', 'W', 'X']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 8)
+
+    def test_ten_point(self):
+        for letter in ['Z']:
+            with self.subTest(letter=letter):
+                self.assertEqual(self.subject.score_letter(letter), 10)
+
 
 
 if __name__ == '__main__':
